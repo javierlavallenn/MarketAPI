84788f3ba52e2b2b59765d263b7d000aa11a612b
cyberbullarg <juan.lavallen@C11-VFOMD4548QK>
2023-12-29 11:07:50 -0300

    se agrega ejemplos de validacion de fluent api para la entidad product

---
+++ Market/Context/Configurations/ProductConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Market.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Market.Context.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Market/Context/MarketContext.cs
+++ Market/Context/MarketContext.cs
@@ -6,5 +6,21 @@ namespace Market.Context
     public class MarketContext(DbContextOptions<MarketContext> options) : DbContext(options)
     {
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Product
+
+            modelBuilder.Entity<Product>()
+                        .Property(p => p.Name)
+                        .IsRequired();
+
+            modelBuilder.Entity<Product>().Property(p => p.Description).IsRequired().HasMaxLength(250);
+            
+            #endregion
+
+            #region Category
+            #endregion
+        }
     }
 }
